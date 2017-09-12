4ab4e0262e36527f4de95cddbc037c5d450c2434
nehacp <neha.chaudhary@hotmail.com>
2017-09-12 12:53:55 -0700

    videoPlayer plays video on click

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,12 @@ angular.module('video-player')
 .component('app', {
 
   controller: function () {
+    console.log('app', this);
     this.videos = window.exampleVideoData;
     this.currentVideo = this.videos[0];
+    this.selectVideo = (index) => {
+      this.currentVideo = this.videos[index];
+    };
   },
    
   templateUrl: 'src/templates/app.html'
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,7 +1,8 @@
 angular.module('video-player')
 .component('videoList', {
   bindings: {
-    videos: '<'
+    videos: '<',
+    onClick: '<'
   },
   
   controller: function() {
