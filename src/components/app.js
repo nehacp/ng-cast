0f33956f61ac240ef3c3e8f62823e8ddb7033550
nehacp <neha.chaudhary@hotmail.com>
2017-09-12 13:17:38 -0700

    add search feature

--- src/components/app.js
+++ src/components/app.js
@@ -5,10 +5,16 @@ angular.module('video-player')
   controller: function () {
     console.log('app', this);
     this.videos = window.exampleVideoData;
-    this.currentVideo;
+    this.currentVideo = {};
     this.selectVideo = (index) => {
       this.currentVideo = this.videos[index];
     };
+
+    this.searchResults = (input) => {
+      console.log('input', input);
+      //youtube
+    };
+
   },
    
   templateUrl: 'src/templates/app.html'
--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,18 @@
 angular.module('video-player')
 
 .component('search', {
+  bindings: {
+    result: '<'
+  },
+
+  controller: function () {
+    this.input = '';
+
+    this.onClick = (input) => {
+      this.result(input);
+      this.input = '';
+    };
+  },
+
   templateUrl: 'src/templates/search.html'
 });
